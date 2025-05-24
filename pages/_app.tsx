b5d217ca71589564b5e49198574a8fda8302cd0b
Robert Cartello <88016387+pumphub@users.noreply.github.com>
2025-05-24 08:21:40 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,26 @@
-import '@/styles/globals.css'
-import 'reactflow/dist/style.css';
-import '@fontsource/inter/';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
-import type { AppProps } from 'next/app'
-import { ReactFlowProvider } from 'reactflow'
-import { Wallet } from "@/context/walletContext";
-import { NetworkProvider } from '@/context/configContext';
-import { ModalProvider } from '@/context/modalContext';
-import { DefaultHead } from '@/layouts/DefaultHead';
-import { ThemeProvider } from '@/context/themeContext';
+import { useState, useEffect } from 'react';
 
 export default function SOLTrek({ Component, pageProps }: AppProps) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
-      <Wallet>
-        <ThemeProvider>
-          <ReactFlowProvider>
-            <NetworkProvider>
-              <ModalProvider>
-                <DefaultHead />
-                <Component {...pageProps} />
-              </ModalProvider>
-            </NetworkProvider>
-          </ReactFlowProvider>
-        </ThemeProvider>
-      </Wallet>
-  )
+    <Wallet>
+      <ThemeProvider>
+        <ReactFlowProvider>
+          <NetworkProvider>
+            <ModalProvider>
+              <DefaultHead />
+              <Component {...pageProps} />
+            </ModalProvider>
+          </NetworkProvider>
+        </ReactFlowProvider>
+      </ThemeProvider>
+    </Wallet>
+  );
 }
